d8fc16daa8c44c56920fde88345f870d74769627
Erik Olsen <olsen.erik@ensco.com>
2023-12-04 16:23:57 -0500

    minor bug fixed and added removing datapoints algorithm

--- remove-data.py
+++ remove-data.py
@@ -1,15 +1,20 @@
-
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import metrics
-def remove_datapoints(df, num_entries):
-    df.iat[2,2 ] = np.nan
+import random
+
+
+def remove_datapoints(df, percent_entries):
+    ix = [(row, col) for row in range(df.shape[0]) for col in range(df.shape[1])]
+    for row, col in random.sample(ix, int(round(percent_entries * len(ix)))):
+        df.iat[row, col] = np.nan
+    return df
 
 
 def get_accuracy(df):
-    X = weather_data['precipitation', 'temp_max', 'temp_min', 'wind']
+    X = weather_data[['precipitation', 'temp_max', 'temp_min', 'wind']]
     Y = weather_data['weather']
     X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.25, random_state=1)
 
@@ -20,9 +25,10 @@ def get_accuracy(df):
     return metrics.accuracy_score(y_test, y_pred)
 
 
-weather_data = pd.read_csv("seattle-weather.csv", header=None)
+weather_data = pd.read_csv("seattle-weather.csv")
 
-weather_data_with_holes = remove_datapoints(weather_data)
+weather_data_with_holes = weather_data.copy(deep=True)
+weather_data_with_holes = remove_datapoints(weather_data_with_holes, .2)
 
 print("Accuracy for untoched dataset:", get_accuracy(weather_data))
 
